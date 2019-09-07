d66f6dab37d48af45f3e0197323e1c063b93d184
ClouderBai <32984116+ClouderBai@users.noreply.github.com>
2019-09-07 19:02:20 +0800

    commit v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { HeroComponent } from "./hero/hero.component";
 
 import { AppComponent } from "./app.component";
 
+const Routes = [{ path: "", component: HeroComponent }];
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HeroComponent, RouterModule.forRoot(Routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,9 @@
+import { OnInit, Component } from "@angular/core";
+
+@Component({
+  selector: "app-hero",
+  templateUrl: "./hero.component.html"
+})
+export class HeroComponent implements OnInit {
+  OnInit() {}
+}
