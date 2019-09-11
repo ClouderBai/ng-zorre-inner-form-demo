ed429073ca90a830addde017ec5a1a7ca18e1142
ClouderBai <32984116+ClouderBai@users.noreply.github.com>
2019-09-11 11:46:54 +0800

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { RouterModule } from "@angular/router";
 import { HeroComponent } from "./hero/hero.component";
 import { NgZorroAntdModule } from "ng-zorro-antd";
 import { AppComponent } from "./app.component";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ListComponent } from "./list/list.component";
+import { FormsComponent } from "./forms/forms.component";
 
 const Routes = [
   { path: "", component: HeroComponent },
   { path: "hero", component: HeroComponent },
-  { path: "list", component: ListComponent }
+  { path: "list", component: ListComponent },
+  { path: "forms", component: FormsComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, ListComponent],
+  declarations: [AppComponent, HeroComponent, ListComponent, FormsComponent],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(Routes),
     NgZorroAntdModule
   ],
